9c430836186478753081eb9db02ddaeeaef58981
Abhishek-ab@ios-dev <abhibagela2015@gmail.com>
2026-05-07 16:44:57 +0530

    Initial Commit

---
+++ CometChatClone/CometChatCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CometChatCloneApp.swift
+//  CometChatClone
+//
+//  Created by Abhishek Bagela on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CometChatCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
